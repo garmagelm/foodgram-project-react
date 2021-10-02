ad901a0be7a14056f37e4ee4b82dec50e409799f
Эльмира <yndx-garmagelm@yandex.ru>
2021-10-02 12:12:16 +0300

    win14

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -92,8 +92,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(read_only=True, many=True)
     author = CustomUserSerializer(read_only=True)
     image = Base64ImageField()
-    ingredients = IngredientForRecipe(
-        many=True, read_only=True, source='recipeingredient_set')
+    ingredients = IngredientForRecipeCreate(many=True)
     is_favorited = serializers.SerializerMethodField()
     is_in_shopping_cart = serializers.SerializerMethodField()
 
